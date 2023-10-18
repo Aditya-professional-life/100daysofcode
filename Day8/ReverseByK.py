d48335fb55cb56b46fd72db8c8f96773b69f0f87
Aditya_Kumar <145346754+Aditya-professional-life@users.noreply.github.com>
2023-10-18 19:24:22 +0530

    Add files via upload

--- Day8/ReverseByK.py
+++ Day8/ReverseByK.py
@@ -12,4 +12,24 @@ class Solution:
                 result.append(A[i])
                 max_element=A[i]
         return result[::-1]
+import math
+def main():
+    T=int(input())
+    while(T>0):
+        nk=[int(x) for x in input().strip().split()]
+        N=nk[0]
+        K=nk[1]
+        arr=[int(x) for x in input().strip().split()]
+        
+        ob = Solution()
+        ob.reverseInGroups(arr,N,K)
+        for i in arr:
+            print(i,end=" ")
+        print()
+        T-=1
+
+if __name__=="__main__":
+    main()
+
+
 
